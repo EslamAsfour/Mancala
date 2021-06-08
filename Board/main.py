010eea883e59c452da2ca41cf86df63bd2452d96
Ahmed Khaled <ahmedkhaled11119999@gmail.com>
2021-06-08 20:24:59 +0200

    - added stealing mode.

--- Board/main.py
+++ Board/main.py
@@ -5,12 +5,13 @@ board = Board()
 # board.saveLoad = ''
 
 board.toggleGameStatus(1)
-if board.saveLoad != 'l':
-    board.printPiles()
+
+board.printPiles()
+
 while board.playing:
-    if board.saveLoad == 's':
-        board.saveGame()
-        break
+    # if board.saveLoad == 's':
+    #     board.saveGame()
+    #     break
     if board.winning_player != 0:
         if board.winning_player == 3:
             print("The game ended in draw!")
@@ -19,15 +20,15 @@ while board.playing:
             print("Congratulation Player ", board.winning_player, ", YOU WON!")
             break
 
-    if board.saveLoad == 'l':
-        board.loadGame()
-        board.printPiles()
+    # if board.saveLoad == 'l':
+    #     board.loadGame()
+    #     board.printPiles()
     print("Its Player ", board.player, " turn !")
     board.clicked_index = int(input("Please enter the starting index\n"))
 
     board.prepMove()
     board.printPiles()
-    board.saveLoad = input("Do you wish to save?\n")
+    # board.saveLoad = input("Do you wish to save?\n")
 
 
 
